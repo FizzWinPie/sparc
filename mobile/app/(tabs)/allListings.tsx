667b1f76af9be5b6ad09726f848f17841702b3b9
FizzWinPie <sulixhoni2000@gmail.com>
2025-06-09 15:49:09 -0400

    restored the allListings

--- mobile/app/(tabs)/allListings.tsx
+++ mobile/app/(tabs)/allListings.tsx
@@ -1,15 +1,11 @@
 import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import SearchBar from "@/components/SearchBar";
-import { SignOutButton } from "@/components/SignOutButton";
 
 const allListings = () => {
   return (
     <SafeAreaView>
       <Text>Shows all possible ev listings in the form of a list</Text>
-      <SearchBar />
-      <SignOutButton />
     </SafeAreaView>
   );
 };
