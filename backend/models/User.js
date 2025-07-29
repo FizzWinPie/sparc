33cf5e3b10c3f890d28e22310e1eb47e6504b067
John Suli <151566120+FizzWinPie@users.noreply.github.com>
2025-07-29 15:46:20 -0400

    matched MongoDB _id to clerkId (#11)
    Co-authored-by: ThienTong5797 <thien.htong@gmail.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true},
   email: { type: String, required: true, unique: true },
   clerkId: { type: String, required: true, unique: true },
   firstName: { type: String, required: false, unique: false },
